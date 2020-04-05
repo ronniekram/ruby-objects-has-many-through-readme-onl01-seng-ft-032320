b72066df79114153e650ad97368de957186d5bdb
Arynn Boniface <arynn.boniface@gmail.com>
2020-04-05 18:24:48 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -6,6 +6,11 @@ class Waiter
   def initialize(name, yrs_experience)
     @name = name 
     @yrs_experience = yrs_experience
+    @@all << self 
+  end 
+  
+  def self.all 
+    @@all
   end 
 
 end
